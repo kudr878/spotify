3b7e031a8f2c6f8c2040097ea6e1322869efbda6
kudr878 <kudr878@gmail.com>
2024-09-23 19:37:23 +0300

    album display

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Sidebar from './components/Sidebar'
-import Player from './components/Player'
+import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+import Display from './components/Display';
 
 const App = () => {
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display />
       </div>
       <Player />
+      <audio preload="auto"></audio>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
